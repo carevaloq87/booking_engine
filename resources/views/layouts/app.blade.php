543470092c374479014f88621c2854ae0a8ac0e6
Christian Arevalo <carevaloq87@hotmail.com>
2018-08-30 11:01:58 +1000

    FIx drop down menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,17 +103,13 @@
                         {{ Auth::user()->name }} <span class="caret"></span>
                     </a>
 
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown" role="menu">
+                        <li>
+                            <a class="dropdown-item" href="{{ route('logout') }}">
+                                {{ __('Logout') }}
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             @endguest
             </ul>
