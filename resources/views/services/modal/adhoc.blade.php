a7d90f30ef112388b7f2ac31be790dbfbfe66a6c
Christian Arevalo <carevaloq87@hotmail.com>
2019-01-08 15:39:08 +1100

    Ad close btn to headers

--- resources/views/services/modal/adhoc.blade.php
+++ resources/views/services/modal/adhoc.blade.php
@@ -5,7 +5,12 @@
             <div class="modal-body p-4">
 
                 <div class="row mx-1 mt-2 mb-4 pb-2 border-bottom">
-                    <h5>Adhoc Booking</h5>
+                    <div class="col-11 p-0">
+                        <h5>Adhoc Booking</h5>
+                    </div>
+                    <div class="col-1">
+                        <button class="close" aria-label="Close" data-dismiss="modal" type="button"><span aria-hidden="true">×</span></button>
+                    </div>
                 </div>
                 <service-adhoc :service="sv_id"></service-adhoc>
 
--- resources/views/services/modal/hours.blade.php
+++ resources/views/services/modal/hours.blade.php
@@ -4,8 +4,13 @@
 
             <div class="modal-body p-3">
 
-                <div class="row mx-4 mt-4 mb-2 pb-2 border-bottom">
-                    <h5>Set Hours</h5>
+                <div class="row mx-2 mt-4 mb-2 pb-2 border-bottom">
+                    <div class="col-11 p-0">
+                        <h5>Set Hours</h5>
+                    </div>
+                    <div class="col-1">
+                        <button class="close" aria-label="Close" data-dismiss="modal" type="button"><span aria-hidden="true">×</span></button>
+                    </div>
                 </div>
 
                 <service-hours :service="sv_id"></service-hours>
