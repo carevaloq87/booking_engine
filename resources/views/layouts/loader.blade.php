8bd3bf9bb5389d5f954664157b4cc18691d279da
Christian Arevalo <carevaloq87@hotmail.com>
2019-01-09 14:49:16 +1100

    Avoid dismiss modal on click or esc

--- resources/views/layouts/loader.blade.php
+++ resources/views/layouts/loader.blade.php
@@ -1,4 +1,4 @@
-<div id="contentLoading" class="modal fade" tabindex="-1" role="dialog">
+<div id="contentLoading" class="modal fade" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog loader-container" role="document">
 
         <div class="loader"></div>
