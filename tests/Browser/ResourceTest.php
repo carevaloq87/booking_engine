9dc84046f50b799b7febdf869d45cf1bbc515d9f
Sebastian Currea <sebastian.currea@gmail.com>
2019-01-23 11:42:16 +1100

    Test Availability and unavailability resources and services

--- tests/Browser/ResourceTest.php
+++ tests/Browser/ResourceTest.php
@@ -66,6 +66,80 @@ class ResourceTest extends DuskTestCase
                     ->assertSee('Resource was successfully deleted!');
         });
     }
+    /**
+     * Set days and hours unavailability for a resource
+     *
+     * @return void
+     */
+    public function testSetUnavailability()
+    {
+        // Create the user
+        $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $browser->loginAs($user)
+                    ->visit('/resources/create')
+                    ->type('name', 'Test Dusk Resource')
+                    ->type('phone', str_repeat('4', 13))
+                    ->type('email', 'testdusk@1.com')
+                    ->press('Add')
+                    ->assertPathIs('/resources')
+                    ->assertSee('Resource was successfully added!');
+        });
+        // Set Days
+
+        $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $resource =  Resource::where('name', 'Test Dusk Resource')->first();
+            $browser->loginAs($user)
+                    ->visit('/resources/show/'.$resource->id)
+                    ->clickLink("Days")
+                    ->pause(20000)
+                    ->press('Current Year')
+                    ->drag('#Jun-15', '#Oct-25')
+                    ->screenshot('unavailableDay')
+                    ->press('Submit')
+                    ->pause(5000)
+                    ->assertSee($resource->name);
+        });
+        // Set Hours
+        $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $resource =  Resource::where('name', 'Test Dusk Resource')->first();
+            $browser->loginAs($user)
+                    ->visit('/resources/show/'.$resource->id)
+                    ->clickLink("Hours")
+                    ->pause(20000)
+                    ->press('#hour')
+                    ->drag('#Wed-540', '#Fri-600')
+                    ->press('Submit')
+                    ->pause(5000)
+                    ->assertSee($resource->name);
+        });
+        //Set AdHoc
+      /*  $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $resource =  Resource::where('name', 'Test Dusk Resource')->first();
+            $browser->loginAs($user)
+                    ->visit('/resources/show/'.$resource->id)
+                    ->clickLink("Ad hoc")
+                    ->pause(20000)
+                    ->press('.vdp-datepicker__calendar-button')
+                    ->click('span.cell.day')
+                    ->screenshot('unavailableAdHoc6');
+      });*/
+        //Delete the resource.
+        $this->browse(function ($browser){
+            $resource =  Resource::where('name', 'Test Dusk Resource')->first();
+            $user = User::where('id', 30)->first();
+
+            $browser->loginAs($user)
+                    ->visit('/resources')
+                    ->click('@delete-resource-'.$resource->id)
+                    ->acceptDialog()
+                    ->assertPathIs('/resources')
+                    ->assertSee('Resource was successfully deleted!');
+        });
+    }
 
 
 }
--- tests/Browser/ServiceTest.php
+++ tests/Browser/ServiceTest.php
@@ -68,5 +68,80 @@ class ServiceTest extends DuskTestCase
 
     }
 
+    public function testSetAvailability()
+    {
+        // Create the user
+        $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $browser->loginAs($user)
+                    ->visit('/services/create')
+                    ->type('name', 'Test Dusk Service')
+                    ->type('phone', str_repeat('4', 13))
+                    ->type('duration', '15')
+                    ->type('interpreter_duration','45')
+                    ->press('Add')
+                    ->assertPathIs('/services')
+                    ->assertSee('Service was successfully added!');
+        });
+        // Set Days
+
+        $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $service =  Service::where('name', 'Test Dusk Service')->first();
+            $browser->loginAs($user)
+                    ->visit('/services/show/'.$service->id)
+                    ->clickLink("Days")
+                    ->pause(20000)
+                    ->press('Current Year')
+                    ->drag('#Jun-15', '#Oct-25')
+                    ->screenshot('availableDay')
+                    ->press('Submit')
+                    ->pause(5000)
+                    ->assertSee($service->name);
+        });
+        // Set Hours
+        $this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $service = Service::where('name', 'Test Dusk Service')->first();
+            $browser->loginAs($user)
+                    ->visit('/services/show/'.$service->id)
+                    ->clickLink("Hours")
+                    ->pause(20000)
+                    ->press('#hour')
+                    ->drag('#Wed-540', '#Fri-600')
+                    ->screenshot('availableDayHour')
+                    ->press('Submit')
+                    ->pause(5000)
+                    ->assertSee($service->name);
+        });
+        //Set AdHoc
+        /*$this->browse(function ($browser) {
+            $user = User::where('id', 30)->first();
+            $resource =  Resource::where('name', 'Test Dusk Resource')->first();
+            $browser->loginAs($user)
+                    ->visit('/resources/show/'.$resource->id)
+                    ->clickLink("Hours")
+                    ->pause(20000)
+                    ->drag('#Wed-540', '#Fri-600')
+                    ->screenshot('unavailableHour')
+                    ->press('Submit')
+                    ->pause(5000)
+                    ->assertSee($resource->name);
+        });*/
+        //Delete the resource.
+        $this->browse(function ($browser){
+            $service =  Service::where('name', 'Test Dusk Service')->first();
+            $user = User::where('id', 30)->first();
+
+            $browser->loginAs($user)
+                    ->visit('/services')
+                    ->click('@delete-service-'.$service->id)
+                    ->acceptDialog()
+                    ->assertPathIs('/services')
+                    ->assertSee('Service was successfully deleted!');
+        });
+    }
+
+
 
 }
